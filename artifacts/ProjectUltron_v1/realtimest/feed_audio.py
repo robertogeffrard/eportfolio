f235751433a483c497e1d83bcc0c7233ed9f6b82
robertogeffrard <rgeffrard2020@gmail.com>
2026-04-19 19:50:33 -0400

    Delete artifacts/ProjectUltron_v1/realtimest directory

--- artifacts/ProjectUltron_v1/realtimest/feed_audio.py
+++
@@ -1,2 +0,0 @@
-pip install -r requirements-gpu-torch.txt
-pip install -r requirements-gpu.txt
--- artifacts/ProjectUltron_v1/realtimest/translator.py
+++
@@ -1,3 +0,0 @@
---index-url https://download.pytorch.org/whl/cu128
-torch==2.7.1+cu128
-torchaudio==2.7.1+cu128
